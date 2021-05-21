c9b704b10ac361a2b37c4edc70cf67dcb9618e2d
flaviopessini <flavio.pessini@outlook.com>
2021-05-20 21:45:52 -0300

    Nova rota de usuários criada

--- src/app.js
+++ src/app.js
@@ -6,4 +6,9 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.status(200).send());
 
+app.get('/users', (req, res) => {
+  const data = [{ name: 'Flávio', email: 'abc@gmail.com' }];
+  return res.status(200).json(data);
+});
+
 module.exports = app;

6d5c8dd629ca5b915ff80311ab46133488ad8f89
flaviopessini <flavio.pessini@outlook.com>
2021-05-20 22:27:33 -0300

    Organização de middlewares com consign

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const app = require('express')();
 
-const app = express();
+const consign = require('consign');
 
-app.use(express.json());
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .into(app);
 
 app.get('/', (req, res) => res.status(200).send());
 

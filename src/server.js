b0788816f59442a5b254b77e79edf1e14711f776
flaviopessini <flavio.pessini@outlook.com>
2021-05-20 21:23:06 -0300

    Criado teste de servidor desenvolvimento

--- src/server.js
+++ src/server.js
@@ -1,9 +1,3 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-
-app.get('/', (req, res) => res.status(200).send());
+const app = require('./app');
 
 app.listen(3001);

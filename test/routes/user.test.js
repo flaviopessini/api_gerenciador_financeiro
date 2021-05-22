3c9467d58b0e38c8091dd472e1bffb03cc5a364b
flaviopessini <flavio.pessini@outlook.com>
2021-05-22 17:03:22 -0300

    Teste adaptado para o banco de dados

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,19 +2,26 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+/**
+ * test.skip
+ * test.only
+ */
+
 test('Deve listar todos os usuários', async () => {
   const res = await request(app).get('/users');
   expect(res.status).toBe(200);
-  expect(res.body).toHaveLength(1);
-  expect(res.body[0]).toHaveProperty('name', 'Flávio');
+  expect(res.body.length).toBeGreaterThan(0);
 });
 
 test('Deve inserir novo usuário com sucesso', async () => {
+  const randomEmail = `${Date.now()}@example.com`;
+  const randomName = `John ${Math.floor(Math.random() * 100)}`;
   const data = {
-    name: 'John Doe',
-    email: 'john_doe@gmail.com',
+    name: randomName,
+    email: randomEmail,
+    passwd: '123456',
   };
   const res = await request(app).post('/users').send(data);
   expect(res.status).toBe(201);
-  expect(res.body.name).toBe('John Doe');
+  expect(res.body.name).toBe(randomName);
 });

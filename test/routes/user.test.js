6cb48a3bd82e5f87dd0373dbcd0cdc1eea3590ca
flaviopessini <flavio.pessini@outlook.com>
2021-05-22 16:46:05 -0300

    Alterção na estrutura de testes

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 
-const app = require('../src/app');
+const app = require('../../src/app');
 
 test('Deve listar todos os usuários', async () => {
   const res = await request(app).get('/users');

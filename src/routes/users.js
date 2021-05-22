e99987c8bd3bd5202017b4b55d7113083f84b1ce
flaviopessini <flavio.pessini@outlook.com>
2021-05-22 16:44:15 -0300

    Criado métodos para o banco de dados

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,19 @@
-module.exports = () => {
-  const findAll = (req, res) => {
-    const data = [{ name: 'Flávio', email: 'abc@gmail.com' }];
-    res.status(200).json(data);
+module.exports = (app) => {
+  const findAll = async (req, res) => {
+    const users = await app.db('users').select();
+    return res.status(200).json(users);
   };
 
-  const create = (req, res) => {
+  const create = async (req, res) => {
+    if (req.body == null) {
+      return res.status(400).send();
+    }
+
     const user = req.body;
-    res.status(201).json(user);
+    // Insere um novo registro e retorna tudo o que foi criado.
+    const result = await app.db('users').insert(user, '*');
+
+    return res.status(201).json(result[0]);
   };
 
   return { findAll, create };

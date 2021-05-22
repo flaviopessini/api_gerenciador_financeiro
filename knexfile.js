2bda11d228a8df17a6ab1f9bee5482b0233c839d
flaviopessini <flavio.pessini@outlook.com>
2021-05-22 16:15:53 -0300

    Arquivo de configuração do knex

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    version: '12.5',
+    connection: {
+      host: '192.168.0.150',
+      user: 'postgres',
+      password: 'postgres',
+      database: 'gerenc_financeiro',
+    },
+    migrations: {
+      directory: 'src/migrations',
+    },
+  },
+};

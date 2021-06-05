486b562e67fe836357fa42388c97f73a65378399
flaviopessini <flavio.pessini@outlook.com>
2021-06-05 12:26:51 -0300

    Validação na inserção de nova transferencia

--- src/services/transfer.js
+++ src/services/transfer.js
@@ -1,7 +1,38 @@
+const ValidationError = require('../errors/ValidationErrors');
+
 module.exports = (app) => {
   const find = (filter = {}) => app.db('transfers').where(filter).select();
 
   const save = async (transfer) => {
+    if (!transfer.description) {
+      throw new ValidationError('Descrição é um atributo obrigatório');
+    }
+    if (!transfer.ammount) {
+      throw new ValidationError('Valor é um atributo obrigatório');
+    }
+    if (!transfer.date) {
+      throw new ValidationError('Data é um atributo obrigatório');
+    }
+    if (!transfer.acc_ori_id) {
+      throw new ValidationError('Conta de origem é um atributo obrigatório');
+    }
+    if (!transfer.acc_dest_id) {
+      throw new ValidationError('Conta de destino é um atributo obrigatório');
+    }
+    if (transfer.acc_ori_id === transfer.acc_dest_id) {
+      throw new ValidationError('Não é possível transferir para a mesma conta');
+    }
+
+    const accounts = await app
+      .db('accounts')
+      .whereIn('id', [transfer.acc_dest_id, transfer.acc_ori_id]);
+
+    accounts.forEach((acc) => {
+      if (acc.user_id !== parseInt(transfer.user_id, 10)) {
+        throw new ValidationError(`Conta #${acc.id} não pertence ao usuário`);
+      }
+    });
+
     const result = await app.db('transfers').insert(transfer, '*');
     const transferId = result[0].id;
 

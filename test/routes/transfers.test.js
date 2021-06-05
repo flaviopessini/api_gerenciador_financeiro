3531df6ef5cafb69f6b649be382362e1d791c9d1
flaviopessini <flavio.pessini@outlook.com>
2021-06-05 10:20:29 -0300

    Refatoração nos testes

--- test/routes/transfers.test.js
+++ test/routes/transfers.test.js
@@ -26,29 +26,80 @@ test('Deve listar apenas as transferências do usuário', async () => {
   expect(res.body[0].description).toBe('Transfer #1');
 });
 
-test('Deve inserir uma transferência com sucesso', async () => {
-  const res = await request(app)
-    .post(MAIN_ROUTE)
-    .set('authorization', `bearer ${TOKEN}`)
-    .send({
-      description: 'Regular transfer',
-      user_id: 10000,
-      acc_ori_id: 10000,
-      acc_dest_id: 10001,
-      ammount: 111.11,
-      date: new Date(),
-    });
-  expect(res.status).toBe(201);
-  expect(res.body.description).toBe('Regular transfer');
+// test('Deve inserir uma transferência com sucesso', async () => {
+//   const res = await request(app)
+//     .post(MAIN_ROUTE)
+//     .set('authorization', `bearer ${TOKEN}`)
+//     .send({
+//       description: 'Regular transfer',
+//       user_id: 10000,
+//       acc_ori_id: 10000,
+//       acc_dest_id: 10001,
+//       ammount: 111.11,
+//       date: new Date(),
+//     });
+//   expect(res.status).toBe(201);
+//   expect(res.body.description).toBe('Regular transfer');
 
-  const transactions = await app
-    .db('transactions')
-    .where({ transfer_id: res.body.id });
-  expect(transactions).toHaveLength(2);
-  expect(transactions[0].description).toBe('Transfer to acc #10001');
-  expect(transactions[1].description).toBe('Transfer from acc #10000');
-  expect(transactions[0].ammount).toBe('-111.11');
-  expect(transactions[1].ammount).toBe('111.11');
-  expect(transactions[0].acc_id).toBe(10000);
-  expect(transactions[1].acc_id).toBe(10001);
+//   const transactions = await app
+//     .db('transactions')
+//     .where({ transfer_id: res.body.id });
+//   expect(transactions).toHaveLength(2);
+//   expect(transactions[0].description).toBe('Transfer to acc #10001');
+//   expect(transactions[1].description).toBe('Transfer from acc #10000');
+//   expect(transactions[0].ammount).toBe('-111.11');
+//   expect(transactions[1].ammount).toBe('111.11');
+//   expect(transactions[0].acc_id).toBe(10000);
+//   expect(transactions[1].acc_id).toBe(10001);
+// });
+
+describe('Ao salvar uma transferência válida ...', () => {
+  let transferId;
+  let incoming;
+  let outgoing;
+
+  test('Deve retornar status 201 e os dados da transferência', async () => {
+    const res = await request(app)
+      .post(MAIN_ROUTE)
+      .set('authorization', `bearer ${TOKEN}`)
+      .send({
+        description: 'Regular transfer',
+        user_id: 10000,
+        acc_ori_id: 10000,
+        acc_dest_id: 10001,
+        ammount: 111.11,
+        date: new Date(),
+      });
+    expect(res.status).toBe(201);
+    expect(res.body.description).toBe('Regular transfer');
+    transferId = res.body.id;
+  });
+
+  test('As transações devem ter sido geradas', async () => {
+    const transactions = await app
+      .db('transactions')
+      .where({ transfer_id: transferId })
+      .orderBy('ammount');
+    expect(transactions).toHaveLength(2);
+    [outgoing, incoming] = transactions;
+  });
+
+  test('A transação de saída deve ser negativa', () => {
+    expect(outgoing.description).toBe('Transfer to acc #10001');
+    expect(outgoing.ammount).toBe('-111.11');
+    expect(outgoing.acc_id).toBe(10000);
+    expect(outgoing.type).toBe('O');
+  });
+
+  test('A transação de entrada deve ser positiva', () => {
+    expect(incoming.description).toBe('Transfer from acc #10000');
+    expect(incoming.ammount).toBe('111.11');
+    expect(incoming.acc_id).toBe(10001);
+    expect(incoming.type).toBe('I');
+  });
+
+  test('Ambas devem referenciar a transferência de origem', () => {
+    expect(incoming.transfer_id).toBe(transferId);
+    expect(outgoing.transfer_id).toBe(transferId);
+  });
 });

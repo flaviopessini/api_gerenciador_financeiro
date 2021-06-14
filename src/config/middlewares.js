e138e2a6580b365339ce8979f7578de94e90aef5
flaviopessini <flavio.pessini@outlook.com>
2021-06-14 20:43:49 -0300

    Adicionado CORS ao projeto

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -1,8 +1,16 @@
 const express = require('express');
 // const logger = require('knex-logger');
 
+const cors = require('cors');
+
 module.exports = (app) => {
   app.use(express.json());
   // Implementa o log para as consultas do banco de dados.
   // app.use(logger(app.db));
+
+  app.use(
+    cors({
+      origin: '*',
+    })
+  );
 };

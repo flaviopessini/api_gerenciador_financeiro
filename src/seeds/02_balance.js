6d8ffb9ba626e65940c2e201e15c6dc3753e5a8a
flaviopessini <flavio.pessini@outlook.com>
2021-06-14 17:43:01 -0300

    Seed de balance

--- src/seeds/02_balance.js
+++ src/seeds/02_balance.js
@@ -1,12 +1,24 @@
-
-exports.seed = (knex) {
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = (knex) =>
+  knex('users')
+    .insert([
+      {
+        id: 10100,
+        name: 'User #3',
+        email: 'user_3@example.com',
+        passwd: '$2a$10$rD7/4wT0dOyClbXSh9tCZuzGPXMk6kxrvC3DNmFW9W.f30jhOHote',
+      },
+      {
+        id: 10101,
+        name: 'User #4',
+        email: 'user_4@example.com',
+        passwd: '$2a$10$rD7/4wT0dOyClbXSh9tCZuzGPXMk6kxrvC3DNmFW9W.f30jhOHote',
+      },
+    ])
+    .then(() =>
+      knex('accounts').insert([
+        { id: 10100, name: 'Acc Saldo Principal', user_id: 10100 },
+        { id: 10101, name: 'Acc Saldo Secundário', user_id: 10100 },
+        { id: 10102, name: 'Acc Alternativa 1', user_id: 10101 },
+        { id: 10103, name: 'Acc Alternativa 2', user_id: 10101 },
+      ])
+    );
